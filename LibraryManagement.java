f6a64e3fa53280b5c02ad100f807435183fad1b4
22BCS10687 <lakshyarajranawat123@gmail.com>
2025-03-06 23:04:38 +0530

    Create LibraryManagement.java

---
+++ LibraryManagement.java
@@ -0,0 +1,60 @@
+// File: LibraryManagement.java
+class Book {
+    protected String title;
+    protected String author;
+    protected double price;
+
+    // Constructor
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    // Method to display book details
+    public void displayDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+}
+
+// Fiction subclass
+class Fiction extends Book {
+    public Fiction(String title, String author, double price) {
+        super(title, author, price);
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("Fiction Book Details:");
+        super.displayDetails();
+    }
+}
+
+// Non-Fiction subclass
+class NonFiction extends Book {
+    public NonFiction(String title, String author, double price) {
+        super(title, author, price);
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("Non-Fiction Book Details:");
+        super.displayDetails();
+    }
+}
+
+// Main class for testing
+public class LibraryManagement {
+    public static void main(String[] args) {
+        // Creating book instances
+        Fiction fictionBook = new Fiction("Harry Potter", "J.K. Rowling", 500);
+        NonFiction nonFictionBook = new NonFiction("Sapiens", "Yuval Noah Harari", 700);
+
+        // Display details
+        fictionBook.displayDetails();
+        System.out.println();
+        nonFictionBook.displayDetails();
+    }
+}
