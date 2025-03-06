38493fd75bfe68a5115948df4b92b4629e874a5b
22BCS10687 <lakshyarajranawat123@gmail.com>
2025-03-06 23:06:49 +0530

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,27 @@
+// File: Product.java
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+
+    // Constructor
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    // Method to display product details
+    public void displayProductDetails() {
+        System.out.println("Product Details:");
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Price: " + price);
+    }
+
+    // Main method for testing
+    public static void main(String[] args) {
+        Product product = new Product(101, "Laptop", 75000);
+        product.displayProductDetails();
+    }
+}
