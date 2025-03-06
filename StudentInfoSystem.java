b9b09bf4e93ed65eb944db88c917d94a8e15a19e
22BCS10687 <lakshyarajranawat123@gmail.com>
2025-03-06 23:05:56 +0530

    Create StudentInfoSystem.java

---
+++ StudentInfoSystem.java
@@ -0,0 +1,62 @@
+// File: StudentInfoSystem.java
+abstract class Person {
+    protected String name;
+    protected int age;
+
+    // Constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Abstract method to display details
+    public abstract void displayDetails();
+}
+
+// Student subclass
+class Student extends Person {
+    private int rollNumber;
+
+    public Student(String name, int age, int rollNumber) {
+        super(name, age);
+        this.rollNumber = rollNumber;
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("Student Details:");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Roll Number: " + rollNumber);
+    }
+}
+
+// Teacher subclass
+class Teacher extends Person {
+    private String subject;
+
+    public Teacher(String name, int age, String subject) {
+        super(name, age);
+        this.subject = subject;
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("Teacher Details:");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Subject: " + subject);
+    }
+}
+
+// Main class for testing
+public class StudentInfoSystem {
+    public static void main(String[] args) {
+        Student student = new Student("Alice", 20, 101);
+        Teacher teacher = new Teacher("Mr. Smith", 40, "Mathematics");
+
+        student.displayDetails();
+        System.out.println();
+        teacher.displayDetails();
+    }
+}
